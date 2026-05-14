2f3a47a1ac194730140dd86c5aab86d1ed84ca7b
bonmin <bonmin411@gmail.com>
2026-05-14 17:56:55 +0900

    websocket router 추가

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
-from config import settings
+from app.config import settings
+from app.ws.websocket import router as ws_router
+from app.api.session import router as session_router
 
 app = FastAPI()
 
 if __name__ == "__main__":
     uvicorn.run(
-        app="main:app",
+        app="app.main:app",
         host="0.0.0.0",
         port=settings.APP_PORT,
         reload=True,
     )
+
+app.include_router(ws_router)
+app.include_router(session_router)
